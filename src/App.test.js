696a708b8028bcf383cf8acef55ddf0f494e91f3
theabhayprajapati <88815641+theabhayprajapati@users.noreply.github.com>
2022-09-18 15:53:42 +0530

    added link to url to this

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  /* learn-link */
+  const linkElement = screen.getByTestId('learn-link');
+  expect(linkElement).toContain("abhayprajapati.me");
 });
